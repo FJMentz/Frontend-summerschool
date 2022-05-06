1707499453158cfed271d5303ab0d63314fcd7c7
Francois Mentz <EC833987@standardbank.co.za>
2022-05-06 11:44:56 +0200

    typo

--- src/app/subjects/subjects.component.ts
+++ src/app/subjects/subjects.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {Subject, Subscription} from 'rxjs';
+import {AsyncSubject, BehaviorSubject, ReplaySubject, Subject, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-subjects',
